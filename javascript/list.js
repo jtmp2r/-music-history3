171762b7fc8d437a5d392cafe70b8f47d3acdd96
Teriq Perry <jtmp2r@gmail.com>
2016-05-14 13:29:26 -0500

    firebase added

--- javascript/list.js
+++ javascript/list.js
@@ -1,30 +1,28 @@
+"use strict"
 $(document).ready(function() {
 
 
 	$.ajax({
-			url: "songs.json"
+			url: "https://music-historyjtmp2r.firebaseio.com/songs/.json"
 		}).done(function(data) {
-	    console.log("ajax", data);
 	    loadSongs(data);
 		});
 		
-	$("#showMe").click(function() {	
-		$.ajax({
-			url: "more.json"
-		}).done(function(data) {
-			console.log("ajax", data);
-			loadSongs(data);
-		})
-  })
+	// $("#showMe").click(function() {	
+	// 	$.ajax({
+	// 		url: "more.json"
+	// 	}).done(function(data) {
+	// 		loadSongs(data);
+	// 	})
+ //  })
 
 	function loadSongs(songView) {
-	  for (var i = 0; i < songView.songs.length; i++) {
-      var songData = songView.songs;
+	  for (var i in songView) {
+      var songData = songView;
 	  	$("#list-view").append("<h2>" + songData[i].artist + "</h2>");
 	  	$("#list-view").append("<h3>" + songData[i].album + "</h3>");
 	  	$("#list-view").append("<div>" + songData[i].title + "</div>");
 	  	$("#list-view").append("<button id='delete'>Delete</button>");
-
 	  };
 	}
 
@@ -32,25 +30,34 @@ $(document).ready(function() {
 		$("list-view").remove();
 	});
 
-	$("button").click(function() {
-		$("#showMe").append($("#addMe").val());
-		loadSongs()
+	$("#ohYeah").click(function() {
+		let newSong = {
+			"album": $("#userAlbum").val(),
+			"artist": $("#userArtist").val(),
+			"title": $("#userTitle").val()
+		}
+		$.ajax({
+			url: "https://music-historyjtmp2r.firebaseio.com/songs/.json",
+			type: "POST",
+			data: JSON.stringify(newSong)
+		}).done(function(stuffFromPost) {
+			loadSongs();
+			console.log("it saves", stuffFromPost)
+		})
+		 
 	});
 
+	$("#link-add").click(function() {
+		$("#musicForm").removeClass("hide");
+		$("#main").addClass("hide");
+	})
 
-  
+	$("#link-home").click(function() {
+		$("#main").removeClass("hide");
+		$("#musicForm").addClass("hide");
+	})
 
-  function linkList() {
-  	$("#musicForm").addClass("hide");
-  	if ($("#link-add").click()) {
-			$("#musicForm").removeClass("hide");
-			$("#main").addClass("hide");
-		}else if($("#link-home").click()) {
-			$("#main").removeClass("hide");
-			$("#musicForm").addClass("hide");
-		}
-  }
-	linkList("");
+
 
 })
 
