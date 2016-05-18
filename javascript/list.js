ec6916be9f732e3e1424a6665e27c5a76d8cd6f5
Teriq Perry <jtmp2r@gmail.com>
2016-05-18 18:24:28 -0500

    delete and partial edit

--- javascript/list.js
+++ javascript/list.js
@@ -1,34 +1,27 @@
 "use strict"
 $(document).ready(function() {
 
+	function getSongs() {
+		$.ajax({
+				url: "https://music-historyjtmp2r.firebaseio.com/songs/.json"
+			}).done(function(data) {
+		    loadSongs(data);
+			});
+	}
+	getSongs();
 
-	$.ajax({
-			url: "https://music-historyjtmp2r.firebaseio.com/songs/.json"
-		}).done(function(data) {
-	    loadSongs(data);
-		});
-		
-	// $("#showMe").click(function() {	
-	// 	$.ajax({
-	// 		url: "more.json"
-	// 	}).done(function(data) {
-	// 		loadSongs(data);
-	// 	})
- //  })
 
 	function loadSongs(songView) {
-	  for (var i in songView) {
+		$("#list-view").html("")
+	  for (let i in songView) {
       var songData = songView;
-	  	$("#list-view").append("<h2>" + songData[i].artist + "</h2>");
-	  	$("#list-view").append("<h3>" + songData[i].album + "</h3>");
-	  	$("#list-view").append("<div>" + songData[i].title + "</div>");
-	  	$("#list-view").append("<button id='delete'>Delete</button>");
+	  	$("#list-view").append(`<h2>${songData[i].artist}</h2>`);
+	  	$("#list-view").append(`<h3>${songData[i].album}</h3>`);
+	  	$("#list-view").append(`<div>${songData[i].title}</div>`);
+	  	$("#list-view").append(`<button id="${i}">Delete</button>`);
+	  	$("#list-view").append(`<button class="edit" id="edit_${i}">Edit</button>`);
 	  };
-	}
-
-	$("#delete").click(function() {
-		$("list-view").remove();
-	});
+	} 
 
 	$("#ohYeah").click(function() {
 		let newSong = {
@@ -36,23 +29,72 @@ $(document).ready(function() {
 			"artist": $("#userArtist").val(),
 			"title": $("#userTitle").val()
 		}
+		addSongs();
+	});
+
+	function addSongs() {
 		$.ajax({
 			url: "https://music-historyjtmp2r.firebaseio.com/songs/.json",
 			type: "POST",
 			data: JSON.stringify(newSong)
 		}).done(function(stuffFromPost) {
-			loadSongs();
-			console.log("it saves", stuffFromPost)
+			$("input").val("")
+			getSongs();
 		})
-		 
-	});
+	}
+
+	$("#list-view").click(function(event) {
+		if (event.currentTarget) {
+			let songId = event.target.id;
+			console.log("delete", event.target.id)
+			$.ajax({
+				url: `https://music-historyjtmp2r.firebaseio.com/songs/${songId}.json`,
+				type: "DELETE"
+			}).done(function() {
+				getSongs();
+			})
+		}
+	})
+
+	$("#list-view").on("click", ".edit", function(e) {
+			let id = $(this).attr("id");
+			let thisId = id.split("edit_")
+			console.log('THIS ID', thisId[1]);
+			let realSongId = thisId[1];
+			let editId = 
+			$("#musicForm").removeClass("hide");
+			$("#main").addClass("hide");
+			$.ajax({
+				url: `https://music-historyjtmp2r.firebaseio.com/songs/${realSongId}.json`,
+				type: "GET"
+			}).done(function(song) {
+				$("#userAlbum").val() = songs.album;
+				$("#userArtist").val() = songs.artist;
+				$("#userTitle").val() = songs.title;
+			})
+	})
+
+  function addEdit() {
+  	if ($(this).attr("id")) {
+  		$.ajax({
+  			url: `https://music-historyjtmp2r.firebaseio.com/songs/${editId}.json`,
+  			type: "PUT"
+  		}).done(function() {
+  			getSongs();
+  		})
+  	}else{
+  		addsongs();
+  	}
+  }
+
+
 
 	$("#link-add").click(function() {
 		$("#musicForm").removeClass("hide");
 		$("#main").addClass("hide");
 	})
 
-	$("#link-home").click(function() {
+	$("#link-home, #ohYeah").click(function() {
 		$("#main").removeClass("hide");
 		$("#musicForm").addClass("hide");
 	})
